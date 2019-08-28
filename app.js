f8c03b987ca732345155ae05254dd0a6e005749e
CN72 <cn72@CN72.local>
2019-08-28 16:22:31 +0100

    my finished code

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let keyPress = window.addEventListener("keydown",() => {
+    let code = event.code;
+    let key = event.key;
+    let which = event.which
+
+    let mainDisplay = document.getElementById("event-which-display")
+    let eventKey = document.getElementById("event-key")
+    let eventCode = document.getElementById("event-code")
+    let eventWhich = document.getElementById("event-which")
+
+    mainDisplay.textContent = which
+    eventKey.textContent = key
+    eventCode.textContent = code
+    eventWhich.textContent = which
+} )
