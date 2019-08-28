67d7e37c17f9933198a150b49a673cd7fc86127c
CN72 <cn72@CN72.local>
2019-08-28 16:52:06 +0100

    code for touchscreens

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ let keyPress = window.addEventListener("keydown",() => {
     eventCode.textContent = code
     eventWhich.textContent = which
 } )
+
+if($(window).width()<=794){
+    let touchPress = window.addEventListener(TouchEvent, () =>{
+        let code = event.code;
+    let key = event.key;
+    let which = event.which
+
+    let mainDisplay = document.getElementById("event-which-display")
+    let eventKey = document.getElementById("event-key")
+    let eventCode = document.getElementById("event-code")
+    let eventWhich = document.getElementById("event-which")
+
+    mainDisplay.textContent = which
+    eventKey.textContent = key
+    eventCode.textContent = code
+    eventWhich.textContent = which
+    })
+}
